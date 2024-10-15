769b5acb809f4e79dee5891691a454b51119351a
Ruben Escudero <rubenescudero00@outlook.com>
2024-10-15 19:43:36 +0200

    commit changes

--- ConnectFour/obj/Debug/net8.0/ConnectFour.AssemblyInfo.cs
+++ ConnectFour/obj/Debug/net8.0/ConnectFour.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ada003f585e82399780fc73b211e659c6e104e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec5e76097d0ec54c7c9f6f6185ad54bd26ea3dbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
